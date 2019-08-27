5f5802d31cda2c5bba1aa682fddfb8bdbf7797e0
TianGuisen <375156836@qq.com>
2019-08-27 10:49:14 +0800

    a

--- baselib/build/generated/source/buildConfig/debug/com/lb/baselib/BuildConfig.java
+++ baselib/build/generated/source/buildConfig/debug/com/lb/baselib/BuildConfig.java
@@ -5,6 +5,11 @@ package com.lb.baselib;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.lb.baselib";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "com.lb.baselib";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
